f20ab47d574a85d3242caba76cebaaf595e16b7b
hmimay <108294154+hmimay@users.noreply.github.com>
2023-02-14 23:27:08 +0800

    Add files via upload

---
+++ src/main/java/com/example/womensafetyapp/helpCenters.java
@@ -0,0 +1,14 @@
+package com.example.womensafetyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class helpCenters extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_centers);
+    }
+}
---
+++ src/main/java/com/example/womensafetyapp/loginRegister.java
@@ -0,0 +1,14 @@
+package com.example.womensafetyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class loginRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_register);
+    }
+}
