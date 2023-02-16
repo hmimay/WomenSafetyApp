f8003596994021c2cd594cb1a1b1b139651f6009
hmimay <108294154+hmimay@users.noreply.github.com>
2023-02-16 11:55:53 +0800

    Add files via upload

---
+++ womensafetyapp/fact1.java
@@ -0,0 +1,14 @@
+package com.example.womensafetyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fact1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fact1);
+    }
+}
---
+++ womensafetyapp/loadingScreen.java
@@ -0,0 +1,20 @@
+package com.example.womensafetyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class loadingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_screen);
+    }
+
+    public void openSplash_screen(View view) {
+        startActivity(new Intent(this,splashScreen.class));
+    }
+}
